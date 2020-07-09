7903d74e80df007f9900c795dbf9e2f207fa7b5e
xouaib <41961680+xouaib@users.noreply.github.com>
2020-07-09 01:01:08 +0100

    Update database name

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'demo_DB'
+app.config['MYSQL_DATABASE_DB'] = 'LusArduinoExam'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
